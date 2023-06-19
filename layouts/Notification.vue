67b1b666b234083b8d68babc425dcbcae164f7a5
Geneviève Perron Migneron <info@gen-migneron.com>
2023-06-19 11:07:09 -0400

    Notification component

---
+++ layouts/Notification.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="notification" v-if="this.$store.state.notification">
+    <b-alert :variant="type" :show="show">{{text}}</b-alert>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        type: this.$store.state.notification.type || 'success',
+        show: this.$store.state.notification.show,
+        text: this.$store.state.notification.show
+      }
+    },
+    methods: {
+      
+    }
+  }
+</script>
