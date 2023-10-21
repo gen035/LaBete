834663daba4403a02066a2e2393201f7a423ed2b
Geneviève Perron Migneron <info@gen-migneron.com>
2023-10-20 20:18:22 -0400

    add privacy policy

---
+++ pages/policy.vue
@@ -0,0 +1,69 @@
+<template>
+  <section class="content policy">
+      <section class="container">
+        <div class="row">
+            <div
+              v-html="$prismic.asHtml(content.title)"
+              class="col-md-12"
+            />
+        </div>
+        <div class="row align-items-center justify-content-center">
+          <div class="col-md-8" v-html="$prismic.asHtml(content.content)" />
+        </div>
+      </section>
+  </section>
+</template>
+
+<script>
+  export default {
+    async asyncData({ app, error, store }) {
+      const locale = store.state.i18n.locale;
+      let content = []
+
+      await app.$prismic.api.query(
+        app.$prismic.predicates.at('document.type', 'policy'), {
+           lang: `${locale}-ca`
+        }
+      ).then((response) => {
+        response.results.forEach(result => {
+          content = result.data;
+        });
+      })
+
+      let seo = await app.$prismic.api.getByID(content.seo.id)
+      seo = seo.data;
+
+      if (content) {
+        return {
+          content,
+          seo
+        }
+      } else {
+        error({ statusCode: 404, message: 'Page not found' })
+      }
+    },
+    methods: {
+      formattedTitle(item) {
+        const title = item && item.label && item.label.length > 0 && item.label[0].text.toLowerCase();
+        return title;
+      }
+    },
+    head() {
+      return {
+        title: this.$prismic.asText(this.seo.title),
+        link: [
+        //{ rel: 'canonical', href: `https://<DOMAIN>${this.$prismic.linkResolver(this.document)}` }
+        ],
+        meta: [
+          { hid: 'description', name: 'description', content: this.$prismic.asText(this.seo.description) }
+        ]
+      }
+    },
+    nuxtI18n: {
+      paths: {
+        fr: '/contact',
+        en: '/contact'
+      }
+    },
+  }
+</script>
