763812ea1894a81a010951f5549290f7089953f6
Geneviève Perron Migneron <info@gen-migneron.com>
2026-05-17 00:09:06 -0400

    feat: migrate error page to Nuxt 4 root error.vue

--- error.vue
+++ error.vue
@@ -6,10 +6,10 @@
         <p v-if="error.statusCode === 404">
           {{ $t('error.notFound.text') }}
         </p>
-        <nuxt-link to="/">{{ $t('error.home') }}</nuxt-link>
+        <button @click="goHome()">{{ $t('error.home') }}</button>
         <Media
           classes="error-img"
-          :image="this.getSettings.error_image"
+          :image="mainStore.settings?.error_image"
         />
       </div>
     </div>
@@ -17,19 +17,22 @@
 </template>
 
 <script>
-  import Media from './../components/Media';
-  import { mapGetters } from 'vuex';
+  import { useMainStore } from './stores/main';
+  import Media from './components/Media';
 
   export default {
     props: ['error'],
-    layout: 'error',// you can set a custom layout for the error page
-    computed: {
-      ...mapGetters([
-        "getSettings"
-      ])
-    },
     components: {
       Media
+    },
+    setup() {
+      const mainStore = useMainStore();
+      return { mainStore };
+    },
+    methods: {
+      goHome() {
+        clearError({ redirect: '/' });
+      }
     }
   }
 </script>
